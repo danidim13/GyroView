73ce894822e65c680620d910d23cbae88daf458c
Daniel <danidiaz@gmail.com>
2018-08-20 13:36:08 -0600

    Crear marcadores

---
+++ src/make_markers.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+void CreateArucoMarkers()
+{
+    Mat outputMarker;
+    Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+
+    for(int i = 0; i < 50; i++) {
+        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
+        ostringstream convert;
+        string imageName = "4x4Marker_";
+        convert << imageName << i << ".jpg";
+        imwrite(convert.str(), outputMarker);
+    }
+}
+
+int main(int argc, char** argv )
+{
+    /*if ( argc != 2 )
+    {
+        cout << "Usage: " << argv[0] << " <Image_Path>\n";
+        return -1;
+    }
+    Folder = argv[1];
+    */
+    CreateArucoMarkers();
+    return 0;
+}
