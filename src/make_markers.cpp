4b30e4213dea7ae56d05591cf7debd34af8bf9ac
Daniel <danidiaza@gmail.com>
2018-09-02 17:52:37 -0600

    Generar marcadores

--- src/make_markers.cpp
+++ src/make_markers.cpp
@@ -24,18 +24,12 @@ void CreateArucoMarkers()
         string imageName = "4x4Marker_";
         convert << imageName << i << ".jpg";
         imwrite(convert.str(), outputMarker);
+        cv::
     }
 }
 
 int main(int argc, char** argv )
 {
-    /*if ( argc != 2 )
-    {
-        cout << "Usage: " << argv[0] << " <Image_Path>\n";
-        return -1;
-    }
-    Folder = argv[1];
-    */
     CreateArucoMarkers();
     return 0;
 }
