0a1dd55a1581e2c276f1849cf36e4185baf5f28e
Daniel <danidiaza@gmail.com>
2018-09-02 22:38:13 -0600

    Calibración interactiva

--- src/make_markers.cpp
+++ src/make_markers.cpp
@@ -12,6 +12,37 @@
 using namespace cv;
 using namespace std;
 
+const float calibrationSquareDimention = 0.15; // tamaño de cada cuadro del tablero
+const float ArucoMarkerSize = 0.15; // tamaño del marcador
+const cv::Size chessboardDimensions = cv::Size(6, 9); // Dimensiones del tablero (esquinas internas)
+
+
+void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f>& corners)
+{
+    for (size_t i = 0; i < boardSize.height; i++) {
+        for (size_t j = 0; j < boardSize.width; j++) {
+                corners.push_back(Point3f(j*squareEdgeLength, i*squareEdgeLength, 0.0f));
+        }
+    }
+}
+
+void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundCorners, bool showResults = false)
+{
+    for (vector<Mat>::iterator it = images.begin(); it != images.end(); it++) {
+        vector<Point2f> pointBuf;
+        bool found = findChessboardCorners(*it, Size(9,6), pointBuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE);
+
+        if (found) {
+            allFoundCorners.push_back(pointBuf);
+        }
+
+        if (showResults) {
+            drawChessboardCorners(*it, Size(9, 6), pointBuf, found);
+            imshow("Calibracion", *it);
+            waitKey(0);
+        }
+    }
+}
 
 void CreateArucoMarkers()
 {
@@ -27,8 +58,54 @@ void CreateArucoMarkers()
     }
 }
 
+void InteractiveCalibrate() {
+    Mat frame;
+    Mat drawToFrame;
+    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+
+    Mat distanceCoefficients;
+
+    vector<Mat> savedImages;
+
+    vector<vector<Point2f>> markerCorners, rejectedCandidates;
+
+    VideoCapture vid(0);
+
+    if (!vid.isOpened()) {
+        return;
+    }
+
+    int fps = 20;
+
+    namedWindow("PiCamera", CV_WINDOW_AUTOSIZE);
+
+    while(true) {
+        if (!vid.read(frame))
+            break;
+
+        vector<Vec2f> foundPoints;
+        bool found = false;
+
+        found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE ); // | CV_CALIB_CB_FAST_CHECK
+        frame.copyTo(drawToFrame);
+        drawChessboardCorners(drawToFrame, chessboardDimensions, foundPoints, found);
+
+        if (found) {
+            imshow("PiCamera", drawToFrame);
+
+        } else {
+            imshow("PiCamera", frame);
+        }
+
+        char character = waitKey(1000/fps);
+
+    }
+
+    return;
+}
+
 int main(int argc, char** argv )
 {
-    CreateArucoMarkers();
+    InteractiveCalibrate();
     return 0;
 }
