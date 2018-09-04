5e54ff6566cc293a62de74d74ccfbd2e629f3700
Daniel <danidiaza@gmail.com>
2018-09-04 11:14:27 -0600

    Ejecutable para calibración

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -123,19 +123,6 @@ bool SaveCalibration (string name )
     return false;
 }
 
-void CreateArucoMarkers()
-{
-    Mat outputMarker;
-    Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
-
-    for(int i = 0; i < 50; i++) {
-        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
-        ostringstream convert;
-        string imageName = "4x4Marker_";
-        convert << imageName << i << ".jpg";
-        imwrite(convert.str(), outputMarker);
-    }
-}
 
 int main(int argc, char** argv )
 {
